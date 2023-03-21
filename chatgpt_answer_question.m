d9c5d980e45a45902c91c11950d7b937eaaf3de9
Your Name <you@example.com>
2023-03-20 21:51:13 -0400

    First edits by a human (after testing what was automatically generated so far.)
    Multiple "ChatGPT" revisions with the following prompts.
    
    > Edit the weboptions call so that weboptions.Timeout needs to be set to a higher value.
    
    > edit the variable 'url' to use the endpoint "https://api.openai.com/v1/completions"
    
    > Add the API argument 'model' to the argument 'data' use 'text-davinci-003'
    
    > Remove the section of code that converts to UTF-8.
    
    > Rewrite the function to call read the api key from a text file to use `fileread` instead

--- chatgpt_answer_question.m
+++ chatgpt_answer_question.m
@@ -1,30 +1,24 @@
 % Read the API key from a text file
-file_id = fopen('api_key.txt', 'r');
-apikey = fscanf(file_id, '%s');
-fclose(file_id);
+apikey = fileread('api_key.txt');
 
 % Specify the question and context in which it should be answered
 question = 'What is the capital of France?';
 context = 'France is a country in Europe with a population of over 67 million people. Its official language is French and its currency is the euro. France is known for its iconic landmarks such as the Eiffel Tower and the Louvre Museum.';
 
-% Encode the question and context using UTF-8
-question_encoded = unicode2native(question, 'UTF-8');
-context_encoded = unicode2native(context, 'UTF-8');
-
 % Generate the answer using ChatGPT
-url = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+url = 'https://api.openai.com/v1/completions';
 
 % Specify options for the web request
 header_fields = {'Content-Type' 'application/json'; 'Authorization' ['Bearer ' apikey]};
-options = weboptions('HeaderFields', header_fields);
+options = weboptions('HeaderFields', header_fields, 'Timeout', 60);
 
-% Specify the prompt and number of tokens to generate
-data = struct('prompt', [question_encoded context_encoded], 'max_tokens', 1000);
+% Specify the prompt, number of tokens to generate, and model to use
+data = struct('prompt', [question context], 'max_tokens', 1000, 'model', 'text-davinci-003');
 
 % Send the request to ChatGPT and get the response
 response = webwrite(url, data, options);
 text_encoded = response.choices(1).text;
-text = native2unicode(text_encoded, 'UTF-8');
+text = char(text_encoded);
 
 % Display the answer
 disp(text);
